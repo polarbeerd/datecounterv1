835bf7c38f5125bc68aa7a68055880d550ffe6f1
polarbeerd <polarbeerd@gmail.com>
2024-03-04 10:51:50 +0300

    datecounter v2

--- src/App.js
+++ src/App.js
@@ -12,20 +12,35 @@ function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   const date = new Date();
   date.setDate(date.getDate() + count);
   return (
     <div>
       <div>
-        <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
-        <button onClick={() => setCount((c) => c + step)}>+</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(+e.target.value)}
+        />
+        <span>Step: {step}</span>
       </div>
       <div>
-        <button onClick={() => setStep((c) => c - 1)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((c) => c + 1)}>+</button>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(+e.target.value)}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
+
       <p>
         <span>
           {count === 0
@@ -36,6 +51,11 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
+      <div>
+        {count !== 0 || step !== 1 ? (
+          <button onClick={handleReset}>Reset</button>
+        ) : null}
+      </div>
     </div>
   );
 }
